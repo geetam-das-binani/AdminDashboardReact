461e570c60d8d236b71917769e2543339c38f459
Geetam <geetambinani6@gmail.com>
2024-02-21 15:52:43 +0530

    add users page

--- src/components/DataTable/DataTable.jsx
+++ src/components/DataTable/DataTable.jsx
@@ -2,10 +2,35 @@ import "./datatable.scss";
 import { userColumns, userRows } from "../../dataSource";
 import { DataGrid } from "@mui/x-data-grid";
 const DataTable = () => {
+	const actionsColumns = [
+		{
+			field: "action",
+			headerName: "Action",
+			width: 200,
+			renderCell: (params) => {
+				return (
+					<div className="cellAction">
+						<div
+							className="viewButton"
+							onClick={() => handleView(params.row.id)}
+						>
+							View
+						</div>
+						<div
+							className="deleteButton"
+							onClick={() => handleDelete(params.row.id)}
+						>
+							Delete
+						</div>
+					</div>
+				);
+			},
+		},
+	];
 	return (
 		<DataGrid
 			rows={userRows}
-			columns={userColumns}
+			columns={[...userColumns, ...actionsColumns]}
 			initialState={{
 				pagination: {
 					paginationModel: {
@@ -13,9 +38,9 @@ const DataTable = () => {
 					},
 				},
 			}}
-			pageSizeOptions={[5]}
 			checkboxSelection
-			disableRowSelectionOnClick
+			// rowsPerPageOptions={[10]}
+			pageSizeOptions={[8]}
 		/>
 	);
 };
--- src/pages/Single/Single.jsx
+++ src/pages/Single/Single.jsx
@@ -1,9 +1,56 @@
 import { useParams } from "react-router-dom";
 import "./single.scss";
-
+import React from "react";
+import Chart from "../../components/Chart/Chart";
+import List from "../List/List";
 const Single = () => {
 	const { userId } = useParams();
-	return <div>{userId}</div>;
+	return (
+		<React.Fragment>
+			<div className="singleTop">
+				<div className="left">
+					<div className="editButton">Edit</div>
+					<h1 className="title">Information</h1>
+					<div className="item">
+						<img
+							src="https://images.pexels.com/photos/733872/pexels-photo-733872.jpeg?auto=compress&cs=tinysrgb&dpr=3&h=750&w=1260"
+							alt="userimage"
+							className="itemImg"
+						/>
+						<div className="details">
+							<h1 className="itemTitle">Jane Doe</h1>
+							<div className="detailItem">
+								<div className="itemKey">Email:</div>
+								<div className="itemValue">janedoe@gmail.com</div>
+							</div>
+							<div className="detailItem">
+								<div className="itemKey">Phone:</div>
+								<div className="itemValue">+1 2313 64 89</div>
+							</div>
+							<div className="detailItem">
+								<div className="itemKey">Address:</div>
+								<div className="itemValue">
+									Elton St. 234 Garden Yd. NewYork
+								</div>
+							</div>
+							<div className="detailItem">
+								<div className="itemKey">Country:</div>
+								<div className="itemValue">USA</div>
+							</div>
+						</div>
+					</div>
+				</div>
+
+				<div className="right">
+					<Chart title={"User Spending (Last 6 months)"} aspect={3 / 1} />
+				</div>
+			</div>
+			<div className="bottom">
+				<h1 className="title">Last Transactions</h1>
+				<List />
+			</div>
+		</React.Fragment>
+	);
 };
 
 export default Single;
